c8bf18ce47bff266c9c4e4d5573cd8ab14bcb9d7
DARKGITHUBPRO <111252375+DARKGITHUBPRO@users.noreply.github.com>
2022-08-16 19:57:21 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+num1 = int (input('   Enter first number : '))
+num2 = int (input('   Enter second number : '))
+o = input ('  Please Enter ooperator : ')
+if o=='+':
+           print ( num1 + num2 )
+if o=='*':
+     print (num1 * num2)
+if o=='/':
+     print  (num1 / num2)
+if o== '-':
+     print (num1 - num2)
+if o=='//':
+     print (num1 //  num2)
